77bf4a1d558a2d5fbbcf9f462226e5a28feb6aef
ririErliza <95483807+ririErliza@users.noreply.github.com>
2023-06-23 09:00:27 +0200

    Update AddNote.jsx

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AddNote() {
+  const[noteText,setNoteText]=useState('');
+
+  const handleChange = (event) =>{
+    // console.log(event.target.value)
+    setNoteText(event.target.value)
+  }
   return (
     <div className='note new'>
         <textarea 
             cols="10" 
             rows="8"
             placeholder='Type to add a note...'
+            onChange={handleChange}
+            value={noteText}
         ></textarea>
         <div className="note-footer">
             <small>... Remaining</small>
