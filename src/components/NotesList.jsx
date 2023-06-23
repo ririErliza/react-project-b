932271c443e21cfe82f3008f570a475040e22565
ririErliza <95483807+ririErliza@users.noreply.github.com>
2023-06-23 09:33:53 +0200

    Update NotesList.jsx

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Note from './Note'
 import AddNote from './AddNote'
 
-export default function NotesList({notes, handleAddNote}) {
+export default function NotesList({notes, handleAddNote, handleDeleteNote}) {
   return (
     <div className='notes-list'>
         {notes.map((note)=> (
 
-         <Note id={note.id} text={note.text} date={note.date}/> 
+         <Note 
+         id={note.id} 
+         text={note.text} 
+         date={note.date}
+         handleDeleteNote={handleDeleteNote}
+         /> 
          ))}
 
          <AddNote handleAddNote={handleAddNote}/>
